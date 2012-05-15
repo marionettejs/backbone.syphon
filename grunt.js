aefb13c613738769c04a82c409f5ff5dc7a03c1b
Derick Bailey <derickbailey@gmail.com>
2012-05-15 14:06:54 -0500

    added grunt based build, and built v0.1.0

---
+++ grunt.js
@@ -0,0 +1,54 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '// Backbone.Syphon, v<%= meta.version %>\n' +
+        '// Copyright (c)<%= grunt.template.today("yyyy") %> Derick Bailey, Muted Solutions, LLC.\n' + 
+        '// Distributed under MIT license\n' + 
+        '// http://github.com/derickbailey/backbone.syphon'
+    },
+    lint: {
+      files: ['src/**/*.js']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:src/backbone.syphon.js>'],
+        dest: 'lib/backbone.syphon.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'lib/backbone.syphon.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: false,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true,
+        Backbone: true,
+        _: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint concat min');
+
+};
