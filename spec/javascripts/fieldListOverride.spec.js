fd50469bd5d0ca29b87fa79d1a9906abab9e05a8
Derick Bailey <derickbailey@gmail.com>
2012-05-17 10:50:54 -0500

    added failing specs to outline next set of features that i need

---
+++ spec/javascripts/fieldListOverride.spec.js
@@ -0,0 +1,22 @@
+describe("override the list of fields to include or ignore", function(){
+
+  describe("when providing a list of fields to include in the result", function(){
+    it("should include the specified fields", function(){
+      fail;
+    });
+
+    it("should ignore all other fields", function(){
+      fail;
+    });
+  });
+
+  describe("when providing a list of fields to ignore in the result", function(){
+    it("should ignore the specified fields", function(){
+      fail;
+    });
+
+    it("should include all other fields", function(){
+      fail;
+    });
+  });
+});
