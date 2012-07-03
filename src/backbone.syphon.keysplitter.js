e4e54645383480a4b68b457c0ac9fa6de012a6b7
Derick Bailey <derickbailey@gmail.com>
2012-07-02 21:03:54 -0500

    replacing function with regex

--- src/backbone.syphon.keysplitter.js
+++ src/backbone.syphon.keysplitter.js
@@ -1,5 +1,13 @@
+// Backbone.Syphon.KeySplitter
+// ---------------------------
+
+// This function is used to split DOM element keys in to an array
+// of parts, which are then used to create a nested result structure.
+// returning `["foo", "bar"]` results in `{foo: { bar: "value" }}`.
+//
+// Override this method to use a custom key splitter, such as:
+// `<input name="foo.bar.baz">`, `return key.split(".")`
 Backbone.Syphon.KeySplitter = function(key){
-  var sections = key.split("[");
-  matches = _.map(sections, function(section){ return section.replace("]","");});
+  var matches = key.match(/[^\[\]]+/g);
   return matches;
 }
