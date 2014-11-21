4a64221322468eb970111c98c70ee2fb54980025
Adrien Delorme <gratator@gmail.com>
2014-11-21 18:50:06 +0100

    fix(Syphon.KeySplitter): allow one character names

--- src/backbone.syphon.keysplitter.js
+++ src/backbone.syphon.keysplitter.js
@@ -11,7 +11,7 @@ Syphon.KeySplitter = function(key) {
   var matches = key.match(/[^\[\]]+/g);
   var lastKey;
 
-  if (key.indexOf('[]') === key.length - 2) {
+  if (key.length > 1 && key.indexOf('[]') === key.length - 2) {
     lastKey = matches.pop();
     matches.push([lastKey]);
   }
