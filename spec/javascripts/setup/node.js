d96f0f7f4c3b44449ed3d313ef7bfd116fb7d310
Sam Saccone <samccone@gmail.com>
2016-03-12 13:58:58 -0800

    Update jsdom and tests to run

--- spec/javascripts/setup/node.js
+++ spec/javascripts/setup/node.js
@@ -1,14 +1,14 @@
 if (!global.document || !global.window) {
   var jsdom = require('jsdom').jsdom;
 
-  global.document = jsdom('<html><head><script></script></head><body></body></html>', null, {
+  global.document = jsdom('<html><head><script></script></head><body></body></html>', {
     FetchExternalResources: ['script'],
     ProcessExternalResources: ['script'],
     MutationEvents: '2.0',
     QuerySelector: false
   });
 
-  global.window = document.createWindow();
+  global.window = document.defaultView;
   global.navigator = global.window.navigator;
 
   global.window.Node.prototype.contains = function(node) {
